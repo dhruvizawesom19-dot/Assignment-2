5060eb025d05765eef739adf84186dbba3c2a27e
Dhruv Patel <dhruvizawesom19@gmail.com>
2025-09-20 13:29:23 -0400

    Finished the Oct to Bin function method

--- convert.c
+++ convert.c
@@ -7,7 +7,20 @@
 #include "convert.h"
 
 void oct_to_bin(const char *oct, char *out){
+    if (!oct || !out) return;
+    size_t len = strlen(oct);
+    size_t pos = 0;
 
+    for (size_t i = 0; i < len; i++) {
+        char c = oct[i];
+        if (c < '0' || c > '7') { out[0] = '\0'; return; }
+        int digit = c - '0';
+
+        out[pos++] = (digit & 4) ? '1' : '0';
+        out[pos++] = (digit & 2) ? '1' : '0';
+        out[pos++] = (digit & 1) ? '1' : '0';
+    }
+    out[pos] = '\0';
 }
 
 void oct_to_hex(const char *oct, char *out){
