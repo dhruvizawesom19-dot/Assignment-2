aace49a834063535405fabaece8da0d07b936d41
Dhruv Patel <dhruvizawesom19@gmail.com>
2025-09-20 13:18:57 -0400

    created base skeletons for the required functions

---
+++ convert.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <limits.h>
+#include "convert.h"
+
+void oct_to_bin(const char *oct, char *out){
+
+}
+
+void oct_to_hex(const char *oct, char *out){
+
+}
+
+void hex_to_bin(const char *hex, char *out){
+
+}
+
+void to_sign_magnitude(int32_t n, char *out) {
+
+}
+
+void to_ones_complement(int32_t n, char *out) {
+
+}
+
+void to_twos_complement(int32_t n, char *out) {
+
+}
